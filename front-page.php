db4f36c1320622c671c205b4568f6ddb78990509
Remkus de Vries <remkus@forsite.nu>
2016-06-16 11:42:09 +0200

    Base front-page.php

---
+++ front-page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * MixUp.
+ *
+ * This file adds the landing page template to the MixUp Theme.
+ *
+ * @package MixUp
+ * @author  ThemeMix
+ * @license GPL-2.0+
+ * @link    https://thememix.com/
+ */
+
+//* Force full-width-content layout setting
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
+//* No need to output the title for the frontpage
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+
+add_filter( 'genesis_site_title_wrap', 'mixup_h1_for_site_title' );
+/**
+ * Use h1 for site title
+ */
+function mixup_h1_for_site_title( $wrap ) {
+	return 'h1';
+}
+
+genesis();
